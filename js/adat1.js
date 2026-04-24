8823e209cb7c9fadea64379d7e46a61d2268b58b
Sajat Nev <email@pelda.hu>
2026-04-24 09:55:03 +0200

    done

---
+++ js/adat1.js
@@ -0,0 +1,17 @@
+const termeszettomb=[
+    {
+        "nev":"Balaton",
+        "leiras":"A Balaton egy tó a Dunántúlon, Közép-Európa legnagyobb tava, Magyarország vízrajzának meghatározó eleme. Könnyen felmelegedő, sekély vize kiválóan alkalmassá teszi a fürdésre és sportolásra, élővilága rendkívül gazdag, a táj változatos vulkanikus kúpjaival sok tekintetben egyedi.",
+        "kep":"termeszet1.jpg"
+    },
+    {
+        "nev":"Badacsony",
+        "leiras":"A Badacsony a Tapolcai-medence legmagasabb tanúhegye, amely a Balaton északi partján magasodik Badacsonytomaj mellett. A nagyjából kör alakú hegy kerülete 11 km. Oldalát 280 méteres magasságig – szőlőtermesztésre kiválóan alkalmas – különféle laza üledékek borítják, efelett szürke bazaltsziklák és kőfolyások bukkannak elő az erdő sűrűjéből.",
+        "kep":"termeszet2.jpg"
+    },
+    {
+        "nev":"Bükk",
+        "leiras":"A Bükk-vidék (régebben csak Bükk) az Északi-középhegységben található földrajzi középtáj, Magyarország legnagyobb átlagmagasságú, barlangokban bővelkedő karszthegysége. ",
+        "kep":"termeszet3.jpg"
+    }
+]
---
+++ js/index.js
@@ -0,0 +1,13 @@
+let sz=""
+for (const elem of termeszettomb) {
+    sz+=`
+    <div class="col-sm-6">
+        <div class="kartya">
+            <p class="text-center cimecske">${elem.nev}</p>
+            <p class="text-center"><img class="img-fluid kep" src="kepek/${elem.kep}" alt=""></p>
+            <p class="leiraszoveg">${elem.leiras}</p>
+        </div>
+    </div>
+    `
+}
+document.getElementById('foDiv').innerHTML=sz
