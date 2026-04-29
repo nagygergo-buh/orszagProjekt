d0b0a37eec33e9c742596d08986ae63f998470c0
Sajat Nev <email@pelda.hu>
2026-04-29 11:22:27 +0200

    index_und_color

--- js/index.js
+++ js/index.js
@@ -1,14 +1,31 @@
 let sz=""
-
-    sz+=`
+sz+=`
     <div class="col-sm-12">
        <div class="container-flex">
-       <div class="text">
-           Ausztrália hivatalos nevén az Ausztrál Államszövetség föderatív királyság, mely az ausztrál kontinenst, Tasmania szigetét és számos kisebb szigetet foglal magába. Északról Pápua Új-Guinea, Indonézia és Kelet-Timor; északkelet felől a Salamon-szigetek és Vanuatu; délkelet felől pedig Új-Zéland határolja tengeren. Fővárosa Canberra, és legnagyobb városa Melbourne. Földünk hatodik legnagyobb területű országa és az egyetlen olyan, amely egy egész kontinensre kiterjed, emellett Ausztrália és Óceánia legnagyobb országa is. Az Egyenlítőtől délre, a 10. és a 40. szélességi kör között található.</div>
-           <img id="terkep" src="kepek/terkep.jpg" alt="terkep" style="height:300px">
-           
+            
+            
+                    <div class="text szinsz" >
+                        Ausztrália hivatalos nevén az Ausztrál Államszövetség föderatív királyság, mely az ausztrál kontinenst, Tasmania szigetét és számos kisebb szigetet foglal magába. Északról Pápua Új-Guinea, Indonézia és Kelet-Timor; északkelet felől a Salamon-szigetek és Vanuatu; délkelet felől pedig Új-Zéland határolja tengeren. Fővárosa Canberra, és legnagyobb városa Melbourne. Földünk hatodik legnagyobb területű országa és az egyetlen olyan, amely egy egész kontinensre kiterjed, emellett Ausztrália és Óceánia legnagyobb országa is. Az Egyenlítőtől délre, a 10. és a 40. szélességi kör között található.
+                    </div>
+                
+                    <img id="terkep" src="kepek/terkep.jpg" alt="terkep" style="height:300px">
+                
+                </div>
+                    <div> 
+                        <ul class="szinsz">
+                            <li>Főváros: Canberra</li>
+                            <li>Legnagyobb város: Sydney</li>
+                            <li>Kontinens: Ausztrália</li>
+                            <li>Nyelv: angol</li>
+                            <li>Lakosság: kb. 26 millió</li>
+                        </ul>
+                    </div>
+                <div class="szinsz">
+                    A kontinens 1606-os holland hajósok által történt felfedezését követően 1770-ben keleti felét az angolok követelték és elkezdték betelepíteni ide az elítélteket, hogy velük kolonizálják Új-Dél-Wales területét 1788.
         </div>
-        <div>A kontinens 1606-os holland hajósok által történt felfedezését követően 1770-ben keleti felét az angolok követelték és elkezdték betelepíteni ide az elítélteket, hogy velük kolonizálják Új-Dél-Wales területét 1788.</div>
+    </div>
+    <div>
+        <p></p>
     </div>
     `
 document.getElementById('foDiv').innerHTML=sz
